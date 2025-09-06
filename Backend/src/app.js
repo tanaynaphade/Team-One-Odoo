754416fdf59113b6180d802efeba42a24a0120d2
Saurabh <dhakitesaurabh@gmail.com>
2025-09-06 12:41:13 +0530

    add: user registration api

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,5 +7,7 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(cookieParser())
 
+import userRoute from "./routes/user.routes.js"
+app.use("/api/v1/users", userRoute)
 
 export {app}
